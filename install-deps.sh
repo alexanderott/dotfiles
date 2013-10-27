a860c7eb9c7b4f0a65c600c6bc9d0d4a0043870a
Alexander Ott <contact@alexanderott.at>
2013-10-27 12:31:04 +0100

    Change some aliases and setup workflow

--- install-deps.sh
+++ install-deps.sh
@@ -1,25 +1,14 @@
-# up to you (me) if you want to run this as a file or copy paste at your leisure
 
+# homebrew!
+# you need the code CLI tools YOU FOOL.
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
 
-# https://github.com/jamiew/git-friendly
-# the `push` command which copies the github compare URL to my clipboard is heaven
-#sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
 # https://rvm.io
 # rvm for the rubiess
 #Paul does: curl -L https://get.rvm.io | bash -s stable --ruby
 #I do this one as it adds dependencies as well
-\curl -L https://get.rvm.io | bash -s stable --rails --autolibs=enable
-
-# https://github.com/isaacs/nave
-# needs npm, obviously.
-# TODO: I think i'd rather curl down the nave.sh, symlink it into /bin and use that for initial node install.
-#npm install -g nave
-
-
-# homebrew!
-# you need the code CLI tools YOU FOOL.
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+\curl -L https://get.rvm.io | bash -s stable --rails --autolibs=homebrew
 
 
 # Make sure we’re using the latest Homebrew
@@ -48,10 +37,11 @@ brew doctor
 #brew install narwhal
 
 # Install more recent versions of some OS X tools
-#brew tap homebrew/dupes
-#brew install homebrew/dupes/grep
+brew tap homebrew/dupes
+brew install homebrew/dupes/grep
 brew tap josegonzalez/homebrew-php
 brew install php55
+brew install josegonzalez/php/composer
 
 # These two formulae didn’t work well last time I tried them:
 #brew install homebrew/dupes/vim
@@ -87,14 +77,14 @@ installcask dropbox
 installcask google-chrome
 installcask google-chrome-canary
 #installcask imagealpha
-installcask imageoptim
-installcask iterm2
+#installcask imageoptim
+#installcask iterm2
 #installcask macvim
 #installcask miro-video-converter
 installcask sublime-text-3
 installcask the-unarchiver
 #installcask tor-browser
-#installcask transmission
+installcask transmission
 #installcask ukelele
 #installcask virtualbox
 installcask vlc
@@ -125,22 +115,6 @@ chmod +x ~/z/z.sh
 #git clone git://github.com/jeroenbegyn/VLCControl.git
 
 
-# my magic photobooth symlink -> dropbox. I love it.
-# first move Photo Booth folder out of Pictures
-# then start Photo Booth. It'll ask where to put the library.
-# put it in Dropbox/public
-
-# now you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
-# then you grab public URL and send off your video message in a heartbeat.
-
-
-# for the c alias (syntax highlighted cat)
-#sudo easy_install Pygments
-
-
-# chrome canary as default
-# on a mac you can set chrome canary as your default inside of Safari preferences :)
-
 # My extra stuff (do I need sudo here?)
 # Check gems are up to date without installing documentation
 gem update --system --no-document
@@ -151,8 +125,6 @@ gem install guard-uglify
 gem install guard-livereload
 
 
-# Install jekyll
-# sudo gem install jekyll
 
 # Set symlink for Sublime Text 3
 mkdir ~/bin
@@ -167,3 +139,5 @@ sudo npm install -g bower
 sudo npm install -g grunt-cli
 sudo npm install -g yo
 sudo npm install -g forever
+
+./.osx
